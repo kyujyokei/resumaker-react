d2f874d2699558e5f90f80d7a64037be53613967
kyujyokei <kyujyokei@gmail.com>
2019-01-04 16:32:32 -0800

    Modified navigation header to show current page

--- src/components/Navigation/NavigationHeader.js
+++ src/components/Navigation/NavigationHeader.js
@@ -1,29 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './NavigationHeader.css';
 import { NavLink, Link } from 'react-router-dom';
 import Button from '../UI/Button/Button';
+import {withRouter} from 'react-router-dom';
 
-const navigationHeader = (props) => (
-    <div className={classes.NavigationHeader}>
-        {/* <div className={classes.RightCorner}>
-            { props.isAuth ? <a href="/logout">Logout</a> : <a href="/signup">Signin / Sign Up</a>}
-        </div> */}
-        <div className={classes.Content}>
-        <Link to={'/'}>
-            <h2 className={classes.Title}><b>Resumaker</b>:α</h2>
-        </Link>
-            {props.isAuth ? 
-                <nav className={classes.Nav}>
-                <a href="/exps">Experiences</a>    |  
-                <a href="/me">    Personal Info</a>    |
-                <NavLink to="/resume" exact>    My Resume</NavLink>
-                </nav>
-                : null
-            }
-            {props.isAuth ? <a href="/logout"><Button btnType="Auth">Logout</Button></a> : <a href="/signup"><Button btnType="Auth">Signin / Sign Up</Button></a>}
-            {/* { props.isAuth ? <a className={classes.RightCorner} href="/logout">Logout</a> : <a className={classes.RightCorner} href="/signup">Signin / Sign Up</a>} */}
-        </div>
-    </div>
-);
+class navigationHeader extends Component {
 
-export default navigationHeader;
+    state = {
+        exps: false,
+        me: false,
+        resume: false
+    }
+    
+    render (){
+        console.log('Navigation: ' ,this.state);
+        let path = this.props.location.pathname;
+        return (
+            <div className={classes.NavigationHeader}>
+                {/* <div className={classes.RightCorner}>
+                    { props.isAuth ? <a href="/logout">Logout</a> : <a href="/signup">Signin / Sign Up</a>}
+                </div> */}
+                <div className={classes.Content}>
+                <Link to={'/'}>
+                    <h2 className={classes.Title}><b>Resumaker</b>:α</h2>
+                </Link>
+                    {this.props.isAuth ? 
+                        <nav className={classes.Nav}>
+                        {path === '/exps' ? <a><b>Experiences</b></a> : <a href="/exps">Experiences</a>}    |  
+                        {path === '/me' ? <a><b>Personal Info</b></a>: <a href="/me">Personal Info</a>}    |
+                        {path === '/resume' ? <a><b>Resume</b></a>: <a href="/resume">Resume</a>} 
+                        {/* <NavLink to="/resume" exact>My Resume</NavLink> */}
+                        </nav>
+                        : null
+                    }
+                    {this.props.isAuth ? <a href="/logout"><Button btnType="Auth">Logout</Button></a> : <a href="/signup"><Button btnType="Auth">Signin / Sign Up</Button></a>}
+                    {/* { props.isAuth ? <a className={classes.RightCorner} href="/logout">Logout</a> : <a className={classes.RightCorner} href="/signup">Signin / Sign Up</a>} */}
+                </div>
+            </div>
+        )
+    }
+};
+
+export default withRouter(navigationHeader);
