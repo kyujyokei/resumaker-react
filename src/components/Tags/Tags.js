0332e1358357e4e11eeb8cc7222675bd6ee5179a
kyujyokei <kyujyokei@gmail.com>
2019-01-17 15:53:52 -0800

    Fixed the css bug in exp page

--- src/components/Tags/Tags.js
+++ src/components/Tags/Tags.js
@@ -5,7 +5,7 @@ const Tags = (props) => {
     return(
         
     <div className={classes.Tags}>
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"></link>
+    {/* <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"></link> */}
     <p>
         {props.children}
     </p>
