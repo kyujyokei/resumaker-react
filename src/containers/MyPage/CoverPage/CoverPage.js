50a3d419efb7e8fed0a759b8ed701115cf740128
kyujyokei <kyujyokei@gmail.com>
2019-01-25 17:47:33 -0800

    Changed cover page's image to background image

--- src/containers/MyPage/CoverPage/CoverPage.js
+++ src/containers/MyPage/CoverPage/CoverPage.js
@@ -6,7 +6,7 @@ class CoverPage extends Component {
     render () {
         return (
             <div className={classes.Background}>
-                <img src={TopImage} className={classes.TopImage}/>
+                {/* <img src={TopImage} className={classes.TopImage}/> */}
                 <h1>Welcome!</h1>
                 <h3>This is an alpha version of Resumaker</h3>
                 <p>Please try it out and let me know your feedback!</p>
