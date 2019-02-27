3ed7ec1d0aa6cef852add88a35cfae52f44b5afc
kyujyokei <kyujyokei@gmail.com>
2019-02-26 17:26:38 -0800

    Changed menu to a responsive menu

--- src/components/Navigation/NavigationHeader.js
+++ src/components/Navigation/NavigationHeader.js
@@ -4,6 +4,8 @@ import { NavLink, Link } from 'react-router-dom';
 import Button from '../UI/Button/Button';
 import {withRouter} from 'react-router-dom';
 import { connect } from 'react-redux';
+import ResponsiveMenu from 'react-responsive-navbar';
+import { FaBars, FaWindowClose } from 'react-icons/fa';
 
 
 class navigationHeader extends Component {
@@ -21,26 +23,37 @@ class navigationHeader extends Component {
 
         return (
             <div className={[classes.NavigationHeader, classes[this.props.bgType]].join(' ')}>
-                {/* <div className={classes.RightCorner}>
-                    { props.isAuth ? <a href="/logout">Logout</a> : <a href="/signup">Signin / Sign Up</a>}
-                </div> */}
                 <div className={classes.Content}>
                 <Link to={'/'}>
                     <h2 className={classes.Title}><b>Resumaker</b> :α</h2>
                 </Link>
                     {this.props.isAuthenticated ? 
 
-                        <nav className={[classes.Nav, 'nav__'].join(' ')}>
-                        {path === '/exps' ? <a><b>Experiences</b></a> : <a href="/exps">Experiences</a>}    |  
-                        {path === '/me' ? <a><b>Personal Info</b></a>: <a href="/me">Personal Info</a>}    |
-                        {path === '/resume' ? <a><b>Resume</b></a>: <a href="/resume">Resume</a>} 
-                        {/* <NavLink to="/resume" exact>My Resume</NavLink> */}
-
-                        </nav>
+                        // <nav className={[classes.Nav, 'nav__'].join(' ')}>
+                        // {path === '/exps' ? <a><b>Experiences</b></a> : <a href="/exps">Experiences</a>}    |  
+                        // {path === '/me' ? <a><b>Personal Info</b></a>: <a href="/me">Personal Info</a>}    |
+                        // {path === '/resume' ? <a><b>Resume</b></a>: <a href="/resume">Resume</a>} 
+                        // </nav>
+                        <ul className={[classes.Nav, 'nav__'].join(' ')}>
+                                    <li>{path === '/exps' ? <a><b>Experiences</b></a> : <a href="/exps">Experiences</a>}</li> 
+                                    <li>{path === '/me' ? <a><b>Personal Info</b></a>: <a href="/me">Personal Info</a>}</li>
+                                    <li>{path === '/resume' ? <a><b>Resume</b></a>: <a href="/resume">Resume</a>}</li>
+                                    <li>{this.props.isAuthenticated ? <a href="/logout"><Button btnType="Logout">Logout</Button></a> : <a href="/signup"><Button btnType="Login">Signin / Sign Up</Button></a>}</li>
+                        </ul>
+                        // <ResponsiveMenu
+                        //     menuOpenButton={<FaBars size={30} color="MediumPurple" />}
+                        //     menuCloseButton={<FaWindowClose size={30} color="MediumPurple" />}
+                        //     changeMenuOn="700px"
+                        //     largeMenuClassName="large-menu-classname"
+                        //     smallMenuClassName="small-menu-classname"
+                        //     menu={
+                                
+                        //     }
+                        // />
 
                         : null
                     }
-                    {this.props.isAuthenticated ? <a href="/logout"><Button btnType="Logout">Logout</Button></a> : <a href="/signup"><Button btnType="Login">Signin / Sign Up</Button></a>}
+                    
                     {/* { props.isAuth ? <a className={classes.RightCorner} href="/logout">Logout</a> : <a className={classes.RightCorner} href="/signup">Signin / Sign Up</a>} */}
                 </div>
             </div> 
